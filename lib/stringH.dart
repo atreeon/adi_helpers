c605fd99feb3f30e4027efd39e26290538211fa2
adrian <adrian@atree.onhill>
2019-02-08 12:19:14 +0000

    new functions - Partial application, myMap.map(), findOutsideOfBrackets, splitByIndices

--- lib/stringH.dart
+++ lib/stringH.dart
@@ -33,10 +33,78 @@ Optional<StrPos> bracketPositionRight(String source) {
     if (source[i] == '(') bracketCount++;
     if (source[i] == ')') bracketCount--;
 
-    if (bracketCount == 0) return Optional.fromNullable(StrPos(i + 1, startIndex + 1));
+    if (bracketCount == 0)
+      return Optional.fromNullable(StrPos(i + 1, startIndex + 1));
   }
 
   return Optional.absent();
+}
+
+///Gets the starting positions of the pattern inside brackets
+///inp: aaa,aaaa<aa,a,>aa,a |
+///out: [3,17]
+List<int> findOutsideOfBrackets(
+    BracketType bracketType, String source, String pattern) {
+  var bracket = getBracket(bracketType);
+
+  var level = 0;
+  var x1 = StringBuffer();
+  source.runes.forEach((x) {
+    var char = String.fromCharCode(x);
+    if (char == bracket.start) level++;
+    if (char == bracket.end) level--;
+    x1.write(level == 0 ? char : "¬");
+  });
+  var x2 = x1.toString();
+
+  var rgx = RegExp(pattern);
+  var r = rgx.allMatches(x2).map((x) => x.start).toList();
+  return r;
+}
+
+enum BracketType { angled, curly, square, parenthesis }
+
+///A container to record the starting and ending bracket characters
+class Bracket {
+  final BracketType bracketType;
+  final String start;
+  final String end;
+
+  Bracket(this.bracketType, this.start, this.end);
+}
+
+///The data for Bracket
+final bracketData = [
+  Bracket(BracketType.angled, "<", ">"),
+  Bracket(BracketType.curly, "{", "}"),
+  Bracket(BracketType.square, "[", "]"),
+  Bracket(BracketType.parenthesis, "(", ")"),
+];
+
+///Gets the related bracket data line for the bracket type
+Bracket getBracket(BracketType bracketType) =>
+    bracketData.firstWhere((x) => x.bracketType == bracketType);
+
+///Splits a string at the indices passed in
+List<String> splitByIndices(String source, List<int> positions) {
+  if (positions[0] != 0) positions.insert(0, 0);
+  if (positions[positions.length - 1] != source.length)
+    positions.insert(positions.length, source.length);
+
+  var x1 = List<String>();
+  for (var i = 0; i < positions.length - 1; i++) {
+    x1.add(source.substring(positions[i], positions[i + 1]));
+  }
+
+  return x1;
+}
+
+///Splits a string into two where it finds the pattern
+/// closest to the right of the string
+List<String> splitByLastOf(String source, String pattern) {
+  var i = source.lastIndexOf(pattern);
+  var r = [source.substring(0, i), source.substring(i + 1)];
+  return r;
 }
 
 ///Object to record the start and end positions in a string
--- test/stringH_test.dart
+++ test/stringH_test.dart
@@ -10,15 +10,20 @@ void main() {
     void exp_getInBracketsRight(String input, String expected) =>
         expectGen((x) => getInBracketsRight(x), input, expected);
 
-    test("getInBracketsRight1",
-        () => exp_getInBracketsRight("out b4((inner) outer)out aft", "((inner) outer)"));
+    test(
+        "getInBracketsRight1",
+        () => exp_getInBracketsRight(
+            "out b4((inner) outer)out aft", "((inner) outer)"));
     test("getInBracketsRight2", () => exp_getInBracketsRight(fnDef5, "()"));
-    test("getInBracketsRight3",
-        () => exp_getInBracketsRight("bl(blim) (plumpy(stumpy))", "(plumpy(stumpy))"));
+    test(
+        "getInBracketsRight3",
+        () => exp_getInBracketsRight(
+            "bl(blim) (plumpy(stumpy))", "(plumpy(stumpy))"));
   });
 
   group("firstWord", () {
-    void exp_firstWord(String input, String expected) => expectGen(firstWord, input, expected);
+    void exp_firstWord(String input, String expected) =>
+        expectGen(firstWord, input, expected);
 
     test("firstWord1", () => exp_firstWord("blah blim", "blah"));
     test("firstWord2", () => exp_firstWord("blah<blim> fn", "blah<blim>"));
@@ -31,24 +36,64 @@ void main() {
 
     test(
         "bracketPositionRight1",
-        () => exp_bracketPositionRight(
-            "bl(blim) (plumpy(stumpy))", Optional.fromNullable(StrPos(10, 24))));
+        () => exp_bracketPositionRight("bl(blim) (plumpy(stumpy))",
+            Optional.fromNullable(StrPos(10, 24))));
     test(
         "bracketPositionRight2",
-        () => exp_bracketPositionRight(
-            "sd()fsdf(l1a(l2a)l1b(l2a)l1c", Optional.fromNullable(StrPos(21, 24))));
+        () => exp_bracketPositionRight("sd()fsdf(l1a(l2a)l1b(l2a)l1c",
+            Optional.fromNullable(StrPos(21, 24))));
     test(
         "bracketPositionRight3",
-        () => exp_bracketPositionRight(
-            "sd()fsdf(l1a(l2a)l1b(l2a)l1c)", Optional.fromNullable(StrPos(9, 28))));
-    test("bracketPositionRight4", () => exp_bracketPositionRight("", Optional.absent()));
+        () => exp_bracketPositionRight("sd()fsdf(l1a(l2a)l1b(l2a)l1c)",
+            Optional.fromNullable(StrPos(9, 28))));
+    test("bracketPositionRight4",
+        () => exp_bracketPositionRight("", Optional.absent()));
 
     test("bracketPositionRight5", () {
       var input = "sd()fsdf(l1a(l2a)l1b(l2a)l1c";
       var expected = "l2a";
       var stringInBrackets = bracketPositionRight(input);
-      var result = input.substring(stringInBrackets.value.start, stringInBrackets.value.end);
+      var result = input.substring(
+          stringInBrackets.value.start, stringInBrackets.value.end);
       expect(result, expected);
+    });
+  });
+
+  group("findOutsideOfBrackets", () {
+    void exp_findOutsideOfBrackets(BracketType bracketType, String source,
+        String pattern, List<int> expected) {
+      var r = findOutsideOfBrackets(bracketType, source, pattern);
+      expect(r.toString(), expected.toString());
+    }
+
+    test("1", () {
+      exp_findOutsideOfBrackets(
+          BracketType.angled, "aa{a,a}a<a,a>a,a", ",", [4, 14]);
+    });
+  });
+
+  group("splitByIndices", () {
+    void exp_splitByIndices(
+        String source, List<int> positions, List<String> expected) {
+      var r = splitByIndices(source, positions);
+      expect(r.toString(), expected.toString());
+    }
+
+    test("1", () {
+      exp_splitByIndices(
+          "aa{a,a}a<a,a>a,a", [4, 14], ["aa{a", ",a}a<a,a>a", ",a"]);
+    });
+  });
+
+  group("splitByLastOf", () {
+    void exp_splitByLastOf(
+        String source, String pattern, List<String> expected) {
+      var r = splitByLastOf(source, pattern);
+      expect(r, expected);
+    }
+
+    test("1", () {
+      exp_splitByLastOf("this is a test", " ", ["this is a", "test"]);
     });
   });
 }
