fb1ceaf1c9aa6962f40ad47dd9ba5a0bcc34a752
Atree <atreeonhill@gmail.com>
2020-04-17 12:27:11 +0100

    DateTime extensions

---
+++ test/dateTimeE_test.dart
@@ -0,0 +1,29 @@
+import 'package:adi_helpers/dateE.dart';
+import 'package:adi_helpers/stringH.dart';
+import 'package:test/test.dart';
+
+void main() {
+  var dt = DateTime.utc(1997, 9, 7, 8, 54);
+
+  group("createSpaces", () {
+    test("1", () {
+      var result = dt.format("yyyy/M/d");
+      expect(result, "1997/9/7");
+    });
+
+    test("2", () {
+      var result = dt.dd$MM$yyyy();
+      expect(result, "07/09/1997");
+    });
+
+    test("3", () {
+      var result = dt.d$M$yyyy();
+      expect(result, "7/9/1997");
+    });
+
+    test("4", () {
+      var result = dt.d$M$yyyy_HH$mm();
+      expect(result, "7/9/1997 08:54");
+    });
+  });
+}
