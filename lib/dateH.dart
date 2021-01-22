3f4ee7a9974ed7124e9581cbac14f13cb5e8eac7
Atree <atreeonhill@gmail.com>
2021-01-22 16:20:09 +0000

    Added some comments

--- lib/dateH.dart
+++ lib/dateH.dart
@@ -1,7 +1,9 @@
+///Strip out the time portion from a DateTime value
 DateTime dateOnly(DateTime dt) {
   return new DateTime(dt.year, dt.month, dt.day);
 }
 
+///now, but without the time portion (plus [days] if passed in)
 DateTime nowDateOnly({int days}) {
   var date = dateOnly(new DateTime.now());
 
